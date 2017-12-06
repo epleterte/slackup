ffb94c889df7ff399aabbaaea8ba32ebdc737077
Christian Bryn <chr.bryn@gmail.com>
2017-12-06 15:47:18 +0100

    bump version before building new release

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup
 
 setup(name='pyslackup',
-      version='0.7.5',
+      version='0.7.6',
       license='ISC',
       description='Wrapper for SlackClient',
       long_description=open('README.rst', 'r').read(),
       author='Christian Bryn',
       author_email='chr.bryn@gmail.com',
       url='https://github.com/epleterte/slackup',
-      download_url='https://github.com/epleterte/slackup/archive/0.7.5.tar.gz',
+      download_url='https://github.com/epleterte/slackup/archive/0.7.6.tar.gz',
       platform='Linux',
       packages=['slackup'],
       py_modules=['slackup'],
